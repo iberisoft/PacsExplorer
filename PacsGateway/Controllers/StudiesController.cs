722877551c8c377037057c46e9f2145764590a8d
Pavel Zaytsev <pavel@iberisoft.com>
2020-01-22 08:13:35 +0100

    PACS gateway

---
+++ PacsGateway/Controllers/StudiesController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace PacsGateway.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StudiesController : ControllerBase
+    {
+        readonly DicomService m_DicomService;
+
+        public StudiesController(DicomService dicomService)
+        {
+            m_DicomService = dicomService;
+        }
+
+        [HttpGet("{id}/open")]
+        public async Task<ActionResult<int>> Open(string id, bool move = false)
+        {
+            try
+            {
+                var seriesCount = await m_DicomService.GetSeriesCount(id);
+                if (seriesCount > 0)
+                {
+                    await m_DicomService.OpenStudy(id, move);
+                    return seriesCount;
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+    }
+}
---
+++ PacsGateway/DicomService.cs
@@ -0,0 +1,82 @@
+﻿using Dicom;
+using DicomScu;
+using Microsoft.Extensions.Configuration;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PacsGateway
+{
+    public class DicomService
+    {
+        readonly Settings m_Settings;
+        readonly DicomQrClient m_DicomQrClient;
+
+        public DicomService(IConfiguration config)
+        {
+            m_Settings = config.Get<Settings>();
+            m_DicomQrClient = new DicomQrClient(m_Settings.QrServer.Host, m_Settings.QrServer.Port, m_Settings.QrServer.AeTitle, m_Settings.Client.AeTitle);
+        }
+
+        public async Task<int> GetSeriesCount(string id)
+        {
+            var request = DicomQrClient.CreateSeriesQueryRequest(id, null);
+            var series = await m_DicomQrClient.QueryAsync(request);
+            return series.Count();
+        }
+
+        public async Task OpenStudy(string id, bool move)
+        {
+            DeleteFolder(id);
+            if (!move)
+            {
+                var request = DicomQrClient.CreateStudyGetRequest(id);
+                await m_DicomQrClient.RetrieveAsync(request, Save);
+            }
+            else
+            {
+                var request = DicomQrClient.CreateStudyMoveRequest(id, m_Settings.Client.AeTitle);
+                await m_DicomQrClient.RetrieveAsync(request, Save, m_Settings.Client.Port);
+            }
+            OpenFolder(id);
+        }
+
+        private async Task<bool> Save(DicomDataset dataset)
+        {
+            if (Directory.Exists(m_Settings.StoragePath))
+            {
+                var file = new DicomFile(dataset);
+                var filePath = Path.Combine(m_Settings.StoragePath, dataset.GetString(DicomTag.StudyInstanceUID), dataset.GetString(DicomTag.SOPInstanceUID) + ".dcm");
+                Directory.CreateDirectory(Path.GetDirectoryName(filePath));
+                await file.SaveAsync(filePath);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void OpenFolder(string id)
+        {
+            if (Directory.Exists(m_Settings.StoragePath))
+            {
+                var folderPath = Path.Combine(m_Settings.StoragePath, id);
+                Process.Start(File.Exists(m_Settings.ImageViewerPath) ? m_Settings.ImageViewerPath : "explorer", folderPath);
+            }
+        }
+
+        private void DeleteFolder(string id)
+        {
+            if (Directory.Exists(m_Settings.StoragePath))
+            {
+                var folderPath = Path.Combine(m_Settings.StoragePath, id);
+                if (Directory.Exists(folderPath))
+                {
+                    Directory.Delete(folderPath, true);
+                }
+            }
+        }
+    }
+}
