ef5502a934a98baab689e393c3d7aefa9962d438
Pavel Zaytsev <pavel@iberisoft.com>
2020-06-09 22:05:23 +0200

    Add Configuration button

---
+++ PacsExplorer/ConfigWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace PacsExplorer
+{
+    /// <summary>
+    /// Interaction logic for ConfigWindow.xaml
+    /// </summary>
+    public partial class ConfigWindow : Window
+    {
+        public ConfigWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Submit(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            if (DialogResult == true)
+            {
+                ServerHost.GetBindingExpression(TextBox.TextProperty).UpdateSource();
+                QrServerAeTitle.GetBindingExpression(TextBox.TextProperty).UpdateSource();
+                QrServerPort.GetBindingExpression(TextBox.TextProperty).UpdateSource();
+                StoreServerAeTitle.GetBindingExpression(TextBox.TextProperty).UpdateSource();
+                StoreServerPort.GetBindingExpression(TextBox.TextProperty).UpdateSource();
+                ClientAeTitle.GetBindingExpression(TextBox.TextProperty).UpdateSource();
+                ClientPort.GetBindingExpression(TextBox.TextProperty).UpdateSource();
+            }
+        }
+    }
+}
